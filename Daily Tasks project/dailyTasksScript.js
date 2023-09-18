2e1744fcf963409e85c70739dff0d3435ee94bb5
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2023-09-18 21:45:01 +0300

    Trying the JavaScript watchPosition() method

--- Daily Tasks project/dailyTasksScript.js
+++ Daily Tasks project/dailyTasksScript.js
@@ -1,6 +1,12 @@
 const myTaskContainer = document.querySelector('.task-container');
 const myAddTaskButton = document.querySelector('#add-task-btn');
 
+window.navigator.geolocation.watchPosition(displayPosition);
+
+function displayPosition(position){
+    console.log(position.coords.longitude);
+    console.log(position.coords.latitude);
+}
 let myClicksOnTheAddButton = 0;
 myAddTaskButton.addEventListener('click', () => {
     myTaskContainer.style.display = "none";

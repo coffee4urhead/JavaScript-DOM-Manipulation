abb84d4b2ac3fa12bf143461f3b3d75d4f932b62
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2023-09-20 23:14:15 +0300

    Fixing the JavaScript elements adding and errors

--- Daily Tasks project/dailyTasksScript.js
+++ Daily Tasks project/dailyTasksScript.js
@@ -1,13 +1,20 @@
 let myContentWrapper = document.querySelector('.container');
+
 const myTaskContainer = document.querySelector('.task-container');
+const getComputedStyles = getComputedStyle(myTaskContainer);
+const marTopVal = getComputedStyles.getPropertyValue('--marTop');
+
 const myAddTaskButton = document.querySelector('#add-task-btn');
 const myInputField = document.querySelector('#inp-type-txt');
 const taskTxt = document.querySelector('.task-txt');
 const myCheckbox = document.querySelector('#check-box-inp');
 const deleteTaskBtn = document.querySelector('.del-task-btn');
 
+let marginTopHolder = 0;
 myAddTaskButton.addEventListener('click', () => {
     
+    marginTopHolder += 40;
+
     if(taskTxt.innerText === myInputField.value) {
         alert("Cannot enter the same task again!");
         return;
@@ -16,21 +23,36 @@ myAddTaskButton.addEventListener('click', () => {
         let txtValueOfInputField = myInputField.value;
 
         let newTaskDivContainer = document.createElement('div');
-        newTaskDivContainer.classList.add('.task-container');
+        newTaskDivContainer.classList.add('task-container');
+        newTaskDivContainer.style.setProperty('--marTop', marginTopHolder + "px");
         
         let newTaskCheckbox = document.createElement('input');
         newTaskCheckbox.setAttribute('type', 'checkbox');
         newTaskCheckbox.setAttribute('id', 'check-box-inp');
         newTaskDivContainer.appendChild(newTaskCheckbox);
-
+        
         let newTaskInfo = document.createElement('p');
-        newTaskInfo.classList.add('.task-txt');
+        newTaskInfo.classList.add('task-txt');
+        newTaskCheckbox.addEventListener('click', () => {
+    
+            clicksOnTheCheckbox++;
+            if(clicksOnTheCheckbox == 1){
+                newTaskInfo.style.textDecoration = "line-through";
+            }
+            else {
+                clicksOnTheCheckbox = 0;
+                newTaskInfo.style.textDecoration = "none";
+            }
+        });
         newTaskInfo.innerText = txtValueOfInputField;
         newTaskDivContainer.appendChild(newTaskInfo);
 
         let deleteButtonForAddedTask = document.createElement('button');
-        deleteButtonForAddedTask.classList.add('.del-task-btn');
+        deleteButtonForAddedTask.classList.add('del-task-btn');
         deleteButtonForAddedTask.innerText = 'Del';
+        deleteButtonForAddedTask.addEventListener('click', () => {
+            myContentWrapper.removeChild(newTaskDivContainer);
+        });
         newTaskDivContainer.appendChild(deleteButtonForAddedTask);
 
         myContentWrapper.appendChild(newTaskDivContainer);
@@ -53,9 +75,3 @@ myCheckbox.addEventListener('click', () => {
 deleteTaskBtn.addEventListener('click', () => {
     myContentWrapper.removeChild(myTaskContainer);
 });
-
-//It doesnt add elements based on their classes and ids provided
-
-/*Possible Solutions:
-    1-
-*/

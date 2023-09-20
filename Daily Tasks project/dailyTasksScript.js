d112a40c87c09ce18aa21a0c29e9ba280170436d
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2023-09-20 22:15:53 +0300

    Adding JavaScript changes

--- Daily Tasks project/dailyTasksScript.js
+++ Daily Tasks project/dailyTasksScript.js
@@ -1,12 +1,25 @@
 const myTaskContainer = document.querySelector('.task-container');
 const myAddTaskButton = document.querySelector('#add-task-btn');
+const myInputField = document.querySelector('#inp-type-txt');
+const taskTxt = document.querySelector('.task-txt');
+const myCheckbox = document.querySelector('#check-box-inp');
+const deleteTaskBtn = document.querySelector('.del-task-btn');
 
 function displayPosition(position){
     console.log(position.coords.longitude);
     console.log(position.coords.latitude);
 }
-setInterval(window.navigator.geolocation.watchPosition(displayPosition), 120000);
-let myClicksOnTheAddButton = 0;
+
 myAddTaskButton.addEventListener('click', () => {
-    myTaskContainer.style.display = "none";
+    window.navigator.geolocation.watchPosition(displayPosition);
+    let txtValueOfInputField = myInputField.value;
+    taskTxt.innerText = txtValueOfInputField;
+});
+
+myCheckbox.addEventListener('click', () => {
+    taskTxt.style.textDecoration = "line-through";
+});
+deleteTaskBtn.addEventListener('click', () => {
+    let myContentWrapper = document.querySelector('.container');
+    myContentWrapper.removeChild(myTaskContainer);
 });

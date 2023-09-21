d12b333b08487ceb3432bb19f140c8f8c781b2e2
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2023-09-22 00:04:57 +0300

    Still have work to do

--- Daily Tasks project/dailyTasksScript.js
+++ Daily Tasks project/dailyTasksScript.js
@@ -6,88 +6,106 @@ const marTopVal = getComputedStyles.getPropertyValue('--marTop');
 
 const myAddTaskButton = document.querySelector('#add-task-btn');
 const myInputField = document.querySelector('#inp-type-txt');
-const taskTxt = document.querySelector('.task-txt');
-const myCheckbox = document.querySelector('.check-box-inp');
-const deleteTaskBtn = document.querySelector('.del-task-btn');
+const taskTxt = document.getElementsByTagName('p');
+const myCheckbox = document.getElementsByTagName('input')[1];
+const deleteTaskBtn = document.getElementsByTagName('button')[1];
 
 let marginTopHolder = 0;
 let cssHeightVariable = 50;
 
-localStorage.setItem("taskInstances", 0);
+myAddTaskButton.addEventListener('click', mainAppFunction());
 
-myAddTaskButton.addEventListener('click', () => {
-    
-    marginTopHolder += 40;
-    if(myInputField.value === "") {
-        alert("Cannot enter a unspecified task to do!");
-        return;
-    }
-    
-    if(taskTxt.innerText === myInputField.value) {
-        alert("Cannot enter the same task again!");
-        return;
-    } 
-    
-    else {
-        let instancesBefore = localStorage.getItem("taskInstances");
-        let instToNumber = Number(instancesBefore);
-        instToNumber++;
-        localStorage.setItem("taskInstances", instToNumber.toString());
-        let txtValueOfInputField = myInputField.value;
-        
-        let newTaskListItem = document.createElement('li');
-        newTaskListItem.style.setProperty('--marTop', marginTopHolder + "px");
-        
-        let newTaskCheckbox = document.createElement('input');
-        newTaskCheckbox.setAttribute('type', 'checkbox');
-        newTaskCheckbox.setAttribute('name', 'checkbox-inp-new');
-        newTaskCheckbox.classList.add('check-box-inp');
-        newTaskListItem.appendChild(newTaskCheckbox);
-        
-        let newTaskInfo = document.createElement('p');
-        newTaskInfo.classList.add('task-txt');
-        newTaskCheckbox.addEventListener('click', () => {
-            
-            clicksOnTheCheckbox++;
-            if(clicksOnTheCheckbox == 1){
-                newTaskInfo.style.textDecoration = "line-through";
-            }
-            else {
-                clicksOnTheCheckbox = 0;
-                newTaskInfo.style.textDecoration = "none";
-            }
-        });
-        newTaskInfo.innerText = txtValueOfInputField;
-        newTaskListItem.appendChild(newTaskInfo);
-        
-        let deleteButtonForAddedTask = document.createElement('button');
-        deleteButtonForAddedTask.classList.add('del-task-btn');
-        deleteButtonForAddedTask.innerText = 'Del';
-        deleteButtonForAddedTask.addEventListener('click', () => {
-            myTaskContainer.removeChild(newTaskListItem);
-        });
-        newTaskListItem.appendChild(deleteButtonForAddedTask);
-        
-        myTaskContainer.appendChild(newTaskListItem);
-        
-        cssHeightVariable += 10;
-        myContentWrapper.style.setProperty('height', cssHeightVariable + 'vh');
-    }
-    
-});
+function mainAppFunction() {
+    myAddTaskButton.addEventListener('click', () => {
+
+        marginTopHolder += 40;
+
+        if (myInputField.value === "") {
+            alert("Cannot enter a unspecified task to do!");
+            return;
+        }
+
+        if (taskTxt.innerText === myInputField.value) {
+            alert("Cannot enter the same task again!");
+            return;
+        }
+
+        else {
+            let txtValueOfInputField = myInputField.value;
+
+            let newTaskListItem = document.createElement('li');
+            newTaskListItem.style.setProperty('--marTop', marginTopHolder + "px");
+
+            let newTaskCheckbox = document.createElement('input');
+            newTaskCheckbox.setAttribute('type', 'checkbox');
+            newTaskCheckbox.setAttribute('name', 'checkbox-inp-new');
+            newTaskListItem.appendChild(newTaskCheckbox);
+
+            let newTaskInfo = document.createElement('p');
+            newTaskCheckbox.addEventListener('click', () => {
+
+                clicksOnTheCheckbox++;
+                if (clicksOnTheCheckbox == 1) {
+                    newTaskInfo.style.textDecoration = "line-through";
+                }
+                else {
+                    clicksOnTheCheckbox = 0;
+                    newTaskInfo.style.textDecoration = "none";
+                }
+            });
+            newTaskInfo.innerText = txtValueOfInputField;
+            newTaskListItem.appendChild(newTaskInfo);
+
+            let deleteButtonForAddedTask = document.createElement('button');
+            deleteButtonForAddedTask.innerText = 'Del';
+            deleteButtonForAddedTask.addEventListener('click', () => {
+                myTaskContainer.removeChild(newTaskListItem);
+                storeData();
+            });
+            newTaskListItem.appendChild(deleteButtonForAddedTask);
+
+            myTaskContainer.appendChild(newTaskListItem);
+
+            cssHeightVariable += 10;
+            myContentWrapper.style.setProperty('height', cssHeightVariable + 'vh');
+            storeData();
+        }
+
+    });
+}
+
+myCheckbox.addEventListener('click', addEventListenerToTheCheckbox());
+
 let clicksOnTheCheckbox = 0;
-myCheckbox.addEventListener('click', () => {
-    
-    clicksOnTheCheckbox++;
-    if(clicksOnTheCheckbox == 1){
-        taskTxt.style.textDecoration = "line-through";
-    }
-    else {
-        clicksOnTheCheckbox = 0;
-        taskTxt.style.textDecoration = "none";
-    }
-});
+function addEventListenerToTheCheckbox() {
 
-deleteTaskBtn.addEventListener('click', () => {
-    myContentWrapper.removeChild(myTaskContainer);
-});
+    myCheckbox.addEventListener('click', () => {
+
+        clicksOnTheCheckbox++;
+        if (clicksOnTheCheckbox == 1) {
+            taskTxt.style.textDecoration = "line-through";
+        }
+        else {
+            clicksOnTheCheckbox = 0;
+            taskTxt.style.textDecoration = "none";
+        }
+    });
+}
+
+deleteTaskBtn.addEventListener('click', deleteTaskFunc());
+
+function deleteTaskFunc() {
+    deleteTaskBtn.addEventListener('click', () => {
+        myContentWrapper.removeChild(myTaskContainer);
+    });
+}
+
+function storeData() {
+    localStorage.setItem("tasksElements", myTaskContainer.innerHTML);
+}
+
+function readData() {
+    myTaskContainer.innerHTML = localStorage.getItem("tasksElements");
+}
+
+readData();

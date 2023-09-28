74c1ebc1300ece6cb2d94aac5f18a0a28ea2e197
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2023-09-28 23:29:52 +0300

    Adding some changes

--- Daily Tasks project/gulpfile.js
+++ Daily Tasks project/gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function transpileStyles() {
-    return src('dailyTaskStyles.scss')
+    return src('*.scss')
     .pipe(sass())
     .pipe(dest('cssTranspiledStyles'))
 }
 
 function watchTask() {
-    watch(['dailyTaskStyles.scss'], transpileStyles())
+    watch(['*.scss'], transpileStyles())
 }
 
 exports.default = series(transpileStyles, watchTask)

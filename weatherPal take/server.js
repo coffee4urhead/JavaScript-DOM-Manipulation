e00444fda1ad4a68a6cec8a8c13bb518ec081091
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2024-02-16 00:36:33 +0200

    Creating the server

---
+++ weatherPal take/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.listen(3030, () => {
+    console.log("Currently listening on port 3000!")
+    console.log("wow it started somehow!");
+})

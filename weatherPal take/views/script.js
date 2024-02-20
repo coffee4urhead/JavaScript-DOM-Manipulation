871e4dad40da055a75c273435abf4902cd96b6f0
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2024-02-20 22:15:41 +0200

    Making styles to home, about and weather

--- weatherPal take/views/script.js
+++ weatherPal take/views/script.js
@@ -25,11 +25,3 @@ hiddenCards.forEach(card => observer.observe(card))
 //     })
 // })
 // hiddenCardsTwo.forEach(card => observer.observe(card))
-
-document.addEventListener('mousemove', function (e) {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    cards.forEach(card => {
-        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-    });
-});

c12440311c7832c3d4006deb5a7c69e677d329e7
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2024-02-25 22:43:16 +0200

    Commit

--- weatherPal take/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/', (req, res) => {
-    res.send("Hello index")
-    console.log("Hello there I am here!In the index though")
-})
-
-module.exports = router;

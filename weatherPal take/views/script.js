34b6bed01896e3a4cd5e2a6d44f1c74d8e2f8805
Mihail Mihaylov <112910051+coffee4urhead@users.noreply.github.com>
2024-03-10 19:35:54 +0200

    Adding infinitely spinning controls

--- weatherPal take/views/script.js
+++ weatherPal take/views/script.js
@@ -3,6 +3,46 @@ let hiddenCards = document.querySelectorAll('.hidden-rotator');
 
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
+// let hiddenCardsTwo = document.querySelectorAll('.hidden-translator');
+// const observerTwo = new IntersectionObserver((entris) => {
+//     entris.forEach(entr => {
+    //         if(entr.isIntersecting){
+//             entr.target.classList.add('show-translator');
+//         }
+//         else {
+    //             entr.target.classList.remove('show-translator');
+    //         }
+    //     })
+    // })
+    // hiddenCardsTwo.forEach(card => observer.observe(card))
+const inputNewsText = document.querySelector('#search-news-textField');
+const submitTopicBtn = document.querySelector('#submit-topic-btn');
+
+// returned info representation ================
+let titlePara = document.querySelector('.title');
+let descriptionPara = document.querySelector('.description');
+// ================ ================ ================ ================
+
+submitTopicBtn.addEventListener('click', function() {
+    fetchNewsData(inputNewsText.value);
+});
+
+async function fetchNewsData(topic) {
+    let fetchNewsData = await fetch(`/newsResp/${topic}`);
+    const receivedData = await fetchNewsData.json();
+    console.log(receivedData);
+    
+    let articlesArray = receivedData.articles;
+    titlePara.textContent = articlesArray[articlesArray.length - 1].title
+    descriptionPara.textContent = articlesArray[articlesArray.length - 1].description
+}
+
+function viewArticle() {
+    window.location.href = "article.html";
+}
+// Adding the spinnig animation to the ul list 
+const scrollers = document.querySelectorAll('.scroller');
+
 if (prefersReducedMotion) {
   const settingsLink = document.createElement('a');
   settingsLink.textContent = "Go to Accessibility Settings";
@@ -31,42 +71,20 @@ if (prefersReducedMotion) {
         })
     })
     hiddenCards.forEach(card => observer.observe(card))
+    addAnimation();
 }
+function addAnimation () {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true);
 
-// let hiddenCardsTwo = document.querySelectorAll('.hidden-translator');
-// const observerTwo = new IntersectionObserver((entris) => {
-//     entris.forEach(entr => {
-//         if(entr.isIntersecting){
-//             entr.target.classList.add('show-translator');
-//         }
-//         else {
-//             entr.target.classList.remove('show-translator');
-//         }
-//     })
-// })
-// hiddenCardsTwo.forEach(card => observer.observe(card))
-const inputNewsText = document.querySelector('#search-news-textField');
-const submitTopicBtn = document.querySelector('#submit-topic-btn');
+        const innerScroller = document.querySelector('.scroller__inner');
+        const scrollerContent = Array.from(innerScroller.children);
 
-// returned info representation ================
-let titlePara = document.querySelector('.title');
-let descriptionPara = document.querySelector('.description');
-// ================ ================ ================ ================
+        scrollerContent.forEach(item => {
 
-submitTopicBtn.addEventListener('click', function() {
-    fetchNewsData(inputNewsText.value);
-});
-
-async function fetchNewsData(topic) {
-    let fetchNewsData = await fetch(`/newsResp/${topic}`);
-    const receivedData = await fetchNewsData.json();
-    console.log(receivedData);
-
-    let articlesArray = receivedData.articles;
-    titlePara.textContent = articlesArray[articlesArray.length - 1].title
-    descriptionPara.textContent = articlesArray[articlesArray.length - 1].description
-}
-
-function viewArticle() {
-    window.location.href = "article.html";
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            innerScroller.appendChild(duplicatedItem);
+        })
+    })
 }
